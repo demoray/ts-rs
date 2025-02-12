49ad05aca5599380bf6da4251ad12fa382a4f4b1
Brian Caswell <bcaswell@gmail.com>
2025-02-11 21:10:54 -0500

    multiple updates
    * reduce potential heap churn
    * no longer panic on write failure
    * lock stdin & stdout for perf improvements

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-extern crate chrono;
 use chrono::{DateTime, Utc};
+use std::io::{stdin, stdout, BufRead, BufReader, Write};
+
+const REALLOC_THRESHOLD: usize = 0xFFFF;
 
 fn main() -> std::io::Result<()> {
     let format = if let Some(format) = std::env::args().nth(1) {
@@ -8,14 +10,27 @@ fn main() -> std::io::Result<()> {
         std::env::var("TS_FORMAT").unwrap_or_else(|_| "%Y-%m-%d %H:%M:%S".to_string())
     };
 
+    let mut stdin = BufReader::new(stdin().lock());
+    let mut stdout = stdout().lock();
+
+    let mut input = String::new();
     loop {
-        let mut input = String::new();
-        let size = std::io::stdin().read_line(&mut input)?;
+        let size = stdin.read_line(&mut input)?;
         if size == 0 {
             break;
         }
         let now: DateTime<Utc> = Utc::now();
-        print!("{} {}", now.format(&format), input);
+
+        if stdout
+            .write_fmt(format_args!("{} {}", now.format(&format), input))
+            .is_err()
+        {
+            break;
+        };
+        input.clear();
+        if input.capacity() > REALLOC_THRESHOLD {
+            input = String::new();
+        }
     }
 
     Ok(())
